f71767dd11bc4ee8da09aa665327e589230f882a
MashodRana <mashod0rana@gmail.com>
2021-08-21 19:31:41 +0600

    recorrection

--- js/javascript.js
+++ js/javascript.js
@@ -14,7 +14,6 @@ let totalCost = 0;
 let isPromoUsed = false;
 const promoCode = 'stevekaku';
 
-console.log("hello I am javascript ")
 
 // -------------------------------------------------------
 //                  Helper functions
